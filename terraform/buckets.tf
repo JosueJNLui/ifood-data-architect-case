9c96607d6e8e3b5fc4c8cec55341d3387a2b65e7
JosueJNLui <nlui.josuej@gmail.com>
2026-05-02 16:58:27 -0300

    feat: infrastructure setup for data lake layers (S3 buckets)

---
+++ terraform/buckets.tf
@@ -0,0 +1,25 @@
+################################################################################
+# S3 Buckets
+# https://github.com/terraform-aws-modules/terraform-aws-s3-bucket/tree/master
+###############################################################################
+
+module "s3_buckets" {
+
+  for_each = contains(local.dev_and_prod_envs, terraform.workspace) ? toset(local.layers) : []
+
+  # Module version
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  # Version
+  version = "~> 3.14.0"
+
+  # Bucket name
+  bucket = format("%s-%s-%s-%s-%s", local.project, local.env, local.aws_region, local.aws_account_id, each.key)
+
+  # Additional tags
+  tags = {
+    "project" = local.project,
+    "layer"   = each.value
+  }
+
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "aws_profile" {
+  description = "Local profile to execute the terraform"
+  type        = string
+}
+
+variable "project" {
+  description = "Project's name"
+  type        = string
+}
